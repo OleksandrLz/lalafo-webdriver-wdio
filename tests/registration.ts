a7d7e9830ad6684b630d6c94f3823ecab6504624
Oleksandr Lazarenko <4oleksandrlz@gmail.com>
2019-04-25 14:46:00 +0300

    edit registration test

--- tests/registration.ts
+++ tests/registration.ts
@@ -45,34 +45,31 @@ describe("Registration", function() {
     expect(userProfileName).to.contain(userRegistrationName);
   });
 
-  // it("via Sign Up PopUp should be successful", function() {
-  //   const email = faker.internet.email(undefined, undefined, "lazarenko.com");
-  //   const userRegistrationName = email.slice(0, -14);
+  it("via Sign Up PopUp should be successful", function() {
+    const email = faker.internet.email(undefined, undefined, "lazarenko.com");
+    const userRegistrationName = email.slice(0, -14);
 
-  //   browser.url("/en");
-  //   browser.pause(2000);
-  //   $("span[data-target='#entryModal']").click();
-  //   $("a#sign-up").click();
-  //   browser.pause(2000);
-
-  //   //$("[id*='registrationform-recaptcha-recaptcha']").setValue('03AOLTBLT7AY-ebjTPbEGNHv-m-TBzr7A_lOJg9Zg-YzEe4JgNORoDte99bDVEIL4mHrmdddiL1u_njZ2WUwj_hwt9gZ-olktEdLQbIjFA0TA8yhSTpspIdYEY0F75dtkNv0L0E7hWnumGbtvsuMZhzRKPI4v6CZnJCplHtuxiJP5KPdTZ6ahnaW42aRv6vMEZnJwmWj5WxKALso9aGZ1pOAJfjfGmRjpAq2Dieux0mF60cu1n6p26TdTSQA7lB01FSZeS9uIx2_kuBPO4MW2xuT7X34faajQSBLglmfmRuc94L8FQuBs-vt_zFPn9_MC2kyLW9H-Mr831Q7-LGjPJcjZgPNyX7Gzp7Tbygif8jzswN3NQuqJsd7iPqz4mxUvi2POiWIshjGJSZAFLZxfhIcX89EK-cPJN-rxYTLsI2kLmKRfAQIfAA-Y-2TOsHvOw8uy4zYmF8QKq');
-  //   //$("[id*='registrationform-recaptcha-recaptcha']").click();
+    browser.url("/en");
+    browser.pause(2000);
+    $("span[data-target='#entryModal']").click();
+    $("a#sign-up").click();
+    browser.pause(2000);
     
-  //   $("input#registrationform-username").click();
-  //   $("input#registrationform-username").setValue(userRegistrationName);
+    $("input#registrationform-username").click();
+    $("input#registrationform-username").setValue(userRegistrationName);
 
-  //   $("input#user_email-popup").setValue(email);
-  //   $("input#registrationform-password").setValue(userRegistrationName);
-  //   $("input#registrationform-passwordconfirm").setValue(userRegistrationName);
-  //   $("input[value='Sign up']").click();
-  //   browser.pause(2000);
+    $("input#user_email-popup").setValue(email);
+    $("input#registrationform-password").setValue(userRegistrationName);
+    $("input#registrationform-passwordconfirm").setValue(userRegistrationName);
+    $("input[value='Sign up']").click();
+    browser.pause(2000);
 
-  //   const userProfileName = $(".user-profile > .header-user-name").getText();
+    const userProfileName = $(".user-profile > .header-user-name").getText();
 
-  //   console.log(userRegistrationName, " - userRegistrationName");
-  //   console.log(userProfileName, " - userProfileName");
+    console.log(userRegistrationName, " - userRegistrationName");
+    console.log(userProfileName, " - userProfileName");
 
-  //   expect(userProfileName).to.contain(userRegistrationName);
-  //   browser.pause(2000);
-  // });
+    expect(userProfileName).to.contain(userRegistrationName);
+    browser.pause(2000);
+  });
 });
